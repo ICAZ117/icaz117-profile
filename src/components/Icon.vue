d0814dc5a65c85cf19c05364c97bb3a0e9ce97d5
ICAZ117 <ibraheemcazalas@gmail.com>
2024-08-29 16:41:19 -0400

    Updated icon component to support resume download

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,63 +1,98 @@
 <template>
-	<a v-if="copy" @click="copyDiscord" class="white">
-		<div class="icon">
-			<i :class="icon"></i>
+	<a v-if="resume" href="/Cazalas_Resume.pdf" class="white" download>
+		<div class="icon" title="Resume">
+			<img src="../assets/Resume.svg" class="resumeIcon" />
 		</div>
 	</a>
-	<a v-if="!copy" :href="link" :target="target" class="white">
-		<div class="icon">
-			<i :class="icon"></i>
-		</div>
-	</a>
+	<div v-else>
+		<a v-if="copy" @click="copyDiscord" class="white">
+			<div class="icon">
+				<i :class="icon"></i>
+			</div>
+		</a>
+		<a v-if="!copy" :href="link" :target="target" class="white">
+			<div class="icon">
+				<i :class="icon"></i>
+			</div>
+		</a>
+	</div>
 </template>
 
 <script>
-	export default {
-		props: ["link", "target", "icon", "copy"],
-		methods: {
-			copyDiscord() {
-				navigator.clipboard.writeText(this.link);
-				this.$notify({
-					title: "Copied!",
-					text: "Discord ID has been copied to your clipboard!",
-					type: "success",
-				});
-			},
+export default {
+	props: {
+		link: {
+			type: String,
+			default: "#",
 		},
-	};
+		target: {
+			type: String,
+			default: "_blank",
+		},
+		icon: {
+			type: String,
+			default: "",
+		},
+		copy: {
+			type: Boolean,
+			default: false,
+		},
+		resume: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	methods: {
+		copyDiscord() {
+			navigator.clipboard.writeText(this.link);
+			this.$notify({
+				title: "Copied!",
+				text: "Discord ID has been copied to your clipboard!",
+				type: "success",
+			});
+		},
+	},
+};
 </script>
 
 <style>
-    .white,
-	.white:hover,
-	.white:active,
-	.white:active:focus,
-	.white:focus,
-	.white:visited {
-		color: white !important;
-        text-decoration: none!important;
-	}
+.white,
+.white:hover,
+.white:active,
+.white:active:focus,
+.white:focus,
+.white:visited {
+	color: white !important;
+	text-decoration: none !important;
+}
 
-	.icon {
-		height: 80px !important;
-		width: 80px !important;
-		border: 3px solid white;
-		padding: 10px;
-		border-radius: 50%;
-		transition: all 0.2s ease-in-out;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		align-content: center;
-	}
+.resumeIcon {
+	filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(360deg)
+		brightness(104%) contrast(103%) drop-shadow(-0px -0px 0px #fff)
+		drop-shadow(0px -0px 0px #fff) drop-shadow(0px 0px 0px #fff)
+		drop-shadow(-0px 0.5px 0px #fff);
+}
 
-	.icon:hover {
-		border-color: var(--red);
-		transform: scale(1.1);
-		cursor: pointer;
-	}
+.icon {
+	height: 80px !important;
+	width: 80px !important;
+	border: 3px solid white;
+	padding: 10px;
+	border-radius: 50%;
+	transition: all 0.2s ease-in-out;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	align-content: center;
+}
 
-	.icon i {
-		font-size: 42px;
-	}
+.icon:hover {
+	border-color: var(--red);
+	transform: scale(1.1);
+	cursor: pointer;
+}
+
+.icon i {
+	font-size: 42px;
+}
 </style>
