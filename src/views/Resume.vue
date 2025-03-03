c2c11051d734527dc3b347c539611b6a477bfff9
ICAZ117 <ibraheemcazalas@gmail.com>
2025-03-03 14:40:06 -0500

    Added resume redirect page

---
+++ src/views/Resume.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    // get homepage from store once mounted
+    mounted() {
+        this.$store.dispatch("fetchHomePage");
+        const homePage = this.$store.getters.getHomePage;
+        window.location.href = homePage.resume;
+    }
+}
+</script>
+
+<style>
+
+</style>
