248c31a41d1909b38ceeb2e891680ec363a5d665
ICAZ117 <ibraheemcazalas@gmail.com>
2025-04-01 18:55:14 -0400

    Switched router to history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
@@ -70,7 +70,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 });
 
